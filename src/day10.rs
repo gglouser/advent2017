dc5940141d1588c9521edc5c8671f418a8618fe1
Grant Glouser <gglouser@gmail.com>
2017-12-11 14:25:29 -0800

    Optimized knot hash implementation

--- src/day10.rs
+++ src/day10.rs
@@ -1,41 +1,77 @@
 use std::fmt::Write;
+use std::time::Instant;
 
-fn twist(xs: &mut [u32], k: usize) {
-    if k == 0 { return; }
-    let section = &mut xs[..k];
-    section.reverse();
+fn twist(xs: &mut [u8], len: usize, offset: usize) {
+    if len == 0 { return; }
+
+    let size = xs.len();
+    let end = offset + len;
+    if end >= size {
+        // Range is split across end of slice.
+        // We reverse the full range by dividing it into three parts.
+        // The middle part is reversed in place. The two end parts
+        // (which are chosen to have the same length) are reversed and
+        // then swapped with each other. This reverse-and-swap is faster
+        // than elementwise swapping because slice reverse is fairly
+        // well optimized in the std lib.
+
+        // Find short section, either [offset..] or [..end]
+        let end = end % size;
+        let (front,back) = xs.split_at_mut(offset);
+        let (a,b,c) = if end < size - offset {
+            // Wrapped section is the short one
+            // The three regions are
+            //   offset..offset+end     -- A. reverse and swap with C
+            //   offset+end..size       -- B. reverse
+            //   0..end                 -- C. reverse and swap with A
+            let (p,q) = back.split_at_mut(end);
+            (p, q, &mut front[..end])
+        } else {
+            // Start section (from offset to end) is the short one
+            // The three regions are
+            //   offset..size                   -- A. reverse and swap with C
+            //   0..(end - (size - offset))     -- B. reverse
+            //   (end - (size - offset))..end   -- C. reverse and swap with A
+            let mid = end - (size - offset);
+            let (p,q) = front[..end].split_at_mut(mid);
+            (back, p, q)
+        };
+        a.reverse();
+        b.reverse();
+        c.reverse();
+
+        // nightly has swap_with_slice, but I'm not using nightly!
+        let tmp = c.to_vec();
+        c.copy_from_slice(a);
+        a.copy_from_slice(&tmp);
+
+    } else {
+        // Range is not split
+        xs[offset..end].reverse();
+    }
 }
 
-fn rotate(xs: &mut [u32], k: usize) {
-    if k == 0 { return; }
-    let mut new_xs = xs[k..].to_vec();
-    new_xs.extend_from_slice(&xs[..k]);
-    xs.copy_from_slice(&new_xs);
-}
-
-fn apply_lengths(size: usize, lengths: &[usize], rounds: u32) -> Vec<u32> {
-    let mut elems = (0..size as u32).collect::<Vec<_>>();
+fn apply_lengths(size: usize, lengths: &[usize], rounds: u32) -> Vec<u8> {
+    let mut elems: Vec<u8> = (0..size).map(|x| x as u8).collect();
     let mut skip = 0;
     let mut offset = 0;
 
     for _ in 0..rounds {
         for &l in lengths {
-            twist(&mut elems, l);
-            rotate(&mut elems, (l + skip) % size);
+            twist(&mut elems, l, offset);
             offset = (offset + l + skip) % size;
             skip += 1;
         }
     }
-    rotate(&mut elems, size - offset);
     elems
 }
 
-const SUGAR: &[usize] = &[17, 31, 73, 47, 23];
+const SUGAR: [usize; 5] = [17, 31, 73, 47, 23];
 
 fn prep_key(keystr: &str) -> Vec<usize> {
     let mut key = Vec::with_capacity(keystr.len() + SUGAR.len());
     keystr.bytes().for_each(|b| key.push(b as usize));
-    key.extend(SUGAR);
+    key.extend(&SUGAR);
     key
 }
 
@@ -56,7 +92,7 @@ fn solve(input: &str) -> (u32, String) {
         .map(|m| m.parse().unwrap())
         .collect();
     let xs = apply_lengths(256, &part1_key, 1);
-    let part1 = xs[0] * xs[1];
+    let part1 = xs[0] as u32 * xs[1] as u32;
 
     let part2 = knot_hash(input.trim());
 
@@ -67,6 +103,16 @@ pub fn run(input: &str) {
     let (part1, part2) = solve(input);
     println!("the single round test value is {}", part1);
     println!("the knot hash is {}", part2);
+    
+    const REPS: u32 = 1000;
+    let now = Instant::now();
+    for _ in 0..REPS {
+        knot_hash("AoC 2017");
+    }
+    let e = now.elapsed();
+    println!("{} hashes in {}.{:03}s", REPS, e.as_secs(), e.subsec_nanos() / 1000000);
+    let hps = REPS as f64 / (e.as_secs() as f64 + e.subsec_nanos() as f64 * 1e-9);
+    println!("{:.0} hashes/sec", hps);
 }
 
 #[cfg(test)]
